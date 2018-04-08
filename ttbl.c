d317e982f33d559eb69fcf69125820c7be81c026
Elena Lucherini <eleluche@gmail.com>
2018-04-08 16:01:23 +0200

    Reserve memory for (mock) pages, modify ttbl_init (not fixed)

--- ttbl.c
+++ ttbl.c
@@ -2,36 +2,69 @@
 #include <stdbool.h>
 #include <ttbl_entry.h>
 
-struct uint48 {
-    uint64_t x:48;
-} __attribute__((packed));
+// Define mock page structure
+struct page {
+	uint64_t one;
+	uint64_t two;
+};
 
-struct uint48 ttbr_el1_baddr;
+// Define TTBR0_EL1.Baddr
+unsigned long ttbr_el1_baddr;
+
+// Reserve memory for pages and TTBL
+struct page pages[512];
 
 ttbl_entry ttbl_l1;
 ttbl_entry ttbl_l2;
 ttbl_entry ttbl_l3[512];
 
-void ttbl_init()
+
+
+void ttbl_init(unsigned long map_start, unsigned long map_end)
 {
-	ttbl_entry *p, *addr;
+	ttbl_entry *p, *entry;
 	int i;
+	unsigned long page_addr = map_start;
+	unsigned long addr = (unsigned long) &ttbl_l1;
 	p = ttbl_l3;
-	printf("ttbl_l3 is %p\n", p);
+	ttbl_entry *ttbl = (ttbl_entry *) ttbr_el1_baddr;
+
+	for (i = 0; i < TTBL_L1_ENTRY_CNT; i++) {
+
+	}
+
+	printf("Map start: 0x%012llx. Map end: 0x%012llx\n\n", map_start, map_end);
+
+	while (page_addr < map_end) {
+		
+		page_addr += TTBL_PAGESIZE;
+	}
+
+	ttbl_entry_set_ap(&ttbl_l1, 1);
+	ttbl_entry_set_next_table_addr(&ttbl_l1, addr);
+	printf("ttbl_l1 at %p: 0x%016llx\n", &ttbl_l1, ttbl_l1);
+	addr = 0x111101110;
+	ttbl_entry_set_ap(&ttbl_l2, 1);
+	ttbl_entry_set_next_table_addr(&ttbl_l2, addr);
+	printf("ttbl_l2 at %p: 0x%016llx\n", &ttbl_l2, ttbl_l2);
 	for (i = 0; p <= &ttbl_l3[511]; p++, i++) {
-		addr = p;
-		*addr = 0x0000000000000000LL;
-		//printf("ttbl_l3[i] at %p, addr at %p, p at %p\n", &ttbl_l3[i], addr, p);
-		ttbl_entry_set_ap(addr, (short unsigned int) 1);
-		printf("ttbl_l3 entry #%d at %p: 0x%016llx\n", i, addr, *addr);
+		entry = p;
+		*entry = 0x0ULL;
+		//printf("ttbl_l3[i] at %p, entry at %p, p at %p\n", &ttbl_l3[i], entry, p);
+		ttbl_entry_set_ap(entry, 1);
+		//printf("ttbl_l3 entry #%d at %p: 0x%016llx\n", i, entry, *entry);
 	}
 
 }
 
 int main()
 {
-	ttbl_init();
-	ttbr_el1_baddr.x = 0x56490967b070;
-	printf("TTBR_EL1.BADDR = 0x%012llx\n", ttbr_el1_baddr.x);
+	struct page *pages_start = &pages[0];
+	struct page *pages_end = &pages[511];
+	pages_end++;
+
+	ttbr_el1_baddr = 0x56490967b070;
+	printf("TTBR_EL1.BADDR = 0x%012llx\n", ttbr_el1_baddr);
+	ttbl_init((unsigned long) pages_start, (unsigned long) pages_end);
 	//printf("ttbl_l3 is %p\n", p);
 }
--- ttbl_entry.c
+++ ttbl_entry.c
@@ -3,15 +3,15 @@
 #include <stdio.h>
 
 
-void ttbl_entry_set_invalid (ttbl_entry *addr)
+void ttbl_entry_set_invalid (ttbl_entry *entry)
 {
-	*addr &= TTBL_INVALIDATE_ENTRY_MASK;
+	*entry &= TTBL_INVALIDATE_ENTRY_MASK;
 }
 
-void ttbl_entry_reset (ttbl_entry *addr)
+void ttbl_entry_reset (ttbl_entry *entry)
 {
-	*addr = 0;
-	//memset(*addr, 0, sizeof(ttbl_entry));
+	*entry = 0;
+	//memset(*entry, 0, sizeof(ttbl_entry));
 }
 
 
@@ -25,35 +25,47 @@ void ttbl_entry_reset (ttbl_entry *addr)
  *	0, 1 = R + W
  *	2, 3 = R only
  */
-void ttbl_entry_set_ap (ttbl_entry *addr, short unsigned int ap)
+void ttbl_entry_set_ap (ttbl_entry *entry, short unsigned int ap)
 {
 	if (ap < 0 || ap > 3) {
-		printf("ERROR: Invalid value for AP: %d", ap);
+		printf("ERROR: Invalid value for AP: %d\n", ap);
 		return;
 	}
 	// clear AP bits
-	*addr &= ~TTBL_ST1_LOWER_AP_MASK;
+	*entry &= ~TTBL_ST1_LOWER_AP_MASK;
 	// set AP bits
-	*addr |= (ap << TTBL_ST1_LOWER_AP_SHIFT) & TTBL_ST1_LOWER_AP_MASK;
+	*entry |= (ap << TTBL_ST1_LOWER_AP_SHIFT) & TTBL_ST1_LOWER_AP_MASK;
 }
 
-void ttbl_entry_set_table (ttbl_entry *addr)
+void ttbl_entry_set_next_table_addr(ttbl_entry *entry, unsigned long int addr)
+{
+	if (addr < 0 || addr > (TTBL_TABLE_NEXTTBL_MASK >> TTBL_TABLE_NEXTTBL_SHIFT)) {
+		printf("ERROR: Invalid value for ADDR: %d\n", addr);
+		return;
+	}
+	// clear next table bits
+	*entry &= ~TTBL_TABLE_NEXTTBL_MASK;
+	// set next table bits
+	*entry |= (addr << TTBL_TABLE_NEXTTBL_SHIFT) & TTBL_TABLE_NEXTTBL_MASK;
+}
+
+void ttbl_entry_set_table (ttbl_entry *entry)
 {
 }
 
-bool ttbl_entry_is_valid (ttbl_entry addr)
+bool ttbl_entry_is_valid (ttbl_entry entry)
 {
-	return (addr & TTBL_IS_VALID_ENTRY_MASK);
+	return (entry & TTBL_IS_VALID_ENTRY_MASK);
 }
 
-bool ttbl_entry_is_table (ttbl_entry addr)
+bool ttbl_entry_is_table (ttbl_entry entry)
 {
 	// TODO: check that entry belongs to level 0, 1, 2
-	return addr & TTBL_IS_TABLE_ENTRY_MASK;
+	return entry & TTBL_IS_TABLE_ENTRY_MASK;
 }
 
-bool ttbl_entry_is_block (ttbl_entry addr)
+bool ttbl_entry_is_block (ttbl_entry entry)
 {
 	// TODO: check that entry belongs to level 0, 1, 2
-	return !ttbl_entry_is_table(addr);
+	return !ttbl_entry_is_table(entry);
 }
